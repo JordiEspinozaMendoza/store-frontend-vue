d11420af57d22dce0523ae292d791165d613dfe1
Jordi <jordi8101@gmail.com>
2022-12-02 11:26:00 -0800

    Created components

---
+++ src/components/navbar/index.vue
@@ -0,0 +1,35 @@
+<script>
+import "./styles.css";
+const routes = [
+  {
+    label: "Home",
+    path: "/",
+  },
+  {
+    label: "Register",
+    path: "/register",
+  },
+];
+export default {
+  name: "NavComponent",
+  data() {
+    return {
+      routes,
+    };
+  },
+};
+</script>
+<template>
+  <div class="navbar__container">
+    <div class="navbar__links">
+      <router-link
+        v-for="route in routes"
+        :key="route.label"
+        :to="route.path"
+        class="navbar__link"
+      >
+        {{ route.label }}
+      </router-link>
+    </div>
+  </div>
+</template>
